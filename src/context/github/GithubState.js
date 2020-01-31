3a08c72d0d6d69114379670b01525aaddff05ba5
jbernste <ankbitinfo@gmail.com>
2020-01-30 19:45:30 -0800

    added env and hid token from view

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -10,7 +10,16 @@ import {
   GET_REPOS
 } from '../types';
 
+let githubClientId;
+let githubClientSecret;
 
+if (process.env.NODE_ENV !== 'production') {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
 
 const GithubState = props => {
   const initialState = {
@@ -27,7 +36,7 @@ const GithubState = props => {
     setLoading();
 
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id="jbernste"&client_secret="0e1ebff433d6f951c02bf81eca08241e0c2e2a00"`
+      `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
@@ -41,7 +50,7 @@ const GithubState = props => {
     setLoading();
 
     const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id="jbernste"&client_secret="0e1ebff433d6f951c02bf81eca08241e0c2e2a00"`
+      `https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
@@ -55,7 +64,7 @@ const GithubState = props => {
     setLoading();
 
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id="jbernste"&client_secret="0e1ebff433d6f951c02bf81eca08241e0c2e2a00"`
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
